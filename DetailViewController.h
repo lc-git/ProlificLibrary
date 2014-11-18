f93a884b3478a34ad72b1ff2a321c81abbb15020
Chao Li <lichao0703@gmail.com>
2014-11-17 20:37:17 -0500

    basic storyboard layout

---
+++ DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  ProlificLibrary
+//
+//  Created by Chao Li on 11/17/14.
+//  Copyright (c) 2014 Columbia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ ProlificLibrary/AddViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddViewController.h
+//  ProlificLibrary
+//
+//  Created by Chao Li on 11/17/14.
+//  Copyright (c) 2014 Columbia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddViewController : UIViewController
+
+@end
