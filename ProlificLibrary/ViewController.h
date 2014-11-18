36848693294f3e77ba51ffeb493d5367289cbe9c
Chao Li <lichao0703@gmail.com>
2014-11-17 19:12:30 -0500

    Initial Commit

---
+++ ProlificLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProlificLibrary
+//
+//  Created by Chao Li on 11/17/14.
+//  Copyright (c) 2014 Columbia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProlificLibrary/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ProlificLibrary
+//
+//  Created by Chao Li on 11/17/14.
+//  Copyright (c) 2014 Columbia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
